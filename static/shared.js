8d9aa98c0242db6496d1468ba2c2d7b44fde8a6d
Gurleen Singh <gs585@drexel.edu>
2023-10-31 15:04:33 -0400

    Added spx defs

--- static/shared.js
+++ static/shared.js
@@ -9,3 +9,23 @@ endAlignText = (k) => {
     gsap.to(`#${k}`, {duration: 0, x: `+=${width}`})
     elem.style.textAnchor = "end"
 }
+
+createTemplateDefinition = (svg) => {
+    def = {
+        "description": "Top left with icon",
+        "playserver": "OVERLAY",
+        "playchannel": "1",
+        "playlayer": "7",
+        "webplayout": "7",
+        "steps" : "1",
+        "out": "manual",
+        "uicolor": "2",
+        "dataformat": "json",
+    }
+    def.description = svg.querySelector("title").innerHTML
+    def.DataFields = []
+    svg.querySelectorAll("text").forEach(x => {
+        def.DataFields.push({field: x.id, ftype: "textfield", title: x.id, value: x.querySelector("tspan").innerHTML})
+    })
+    window.SPXGCTemplateDefinition = def
+}
