0dd6f2668a55cb415261483a8a311b0b037421f9
Gurleen Singh <gs585@drexel.edu>
2023-12-16 12:29:07 -0500

    update

--- server/main.py
+++ server/main.py
@@ -4,7 +4,16 @@ import socket_handlers
 import replicant
 from templates.api import router as templates_router
 from rosters import router as rosters_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = ASGIApp(sio, fastapi_app)
 fastapi_app.include_router(templates_router)
 fastapi_app.include_router(rosters_router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
